b304b8ae44942b062514eed13ced3a1ef4af02bc
Vlad Shilov <omgovich@ya.ru>
2020-08-07 12:58:39 +0300

    Add hex2hsv

--- src/hex2hsl.js
+++ src/hex2hsl.js
@@ -1,10 +1,8 @@
 // @flow
-import hex2rgb from './hex2rgb';
-import rgb2hsv from './rgb2hsv';
+import hex2hsv from './hex2hsv';
 import hsv2hsl from './hsv2hsl';
 
 export default function (hex: string) {
-  const { r, g, b } = hex2rgb(hex);
-  const { h, s, v } = rgb2hsv(r, g, b);
+  const { h, s, v } = hex2hsv(hex);
   return hsv2hsl(h, s, v);
 }
